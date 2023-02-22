c178757c261e19ff4c2f3dea36027bad36b75dd4
aandd <aandd@ABREU_DELL>
2023-02-22 16:13:30 -0300

    Módulo 22: 22.13 - Model Login

--- jdev.curso-jsp/src/main/java/servlets/ServletLogin.java
+++ jdev.curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -2,10 +2,12 @@ package servlets;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
 
 //O Chamado Controller são as Servlets ou ServletLoginController
 //@WebServlet("/ServletLogin")  //Mapeamento de url que vem da tela
@@ -26,8 +28,27 @@ public class ServletLogin extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		System.out.println(request.getParameter("nome"));
-		System.out.println(request.getParameter("idade"));
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		//Validar dados da tela
+		if(login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);	
+		}else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe um login e senha corretamente!");
+			redirecionar.forward(request, response);
+		}
+		
+
+		
+		
+		//Precisa de um metodo de autenticação, pegar os parametros e passar os objetos, criar um filtro de login, connexão com o banco
+		
+		
+		
 	}
 
 }
--- jdev.curso-jsp/src/main/webapp/index.jsp
+++ jdev.curso-jsp/src/main/webapp/index.jsp
@@ -3,20 +3,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<title>Curso JSP</title>
+<meta charset="UTF-8">
+<title>Curso JSP</title>
 </head>
 <body>
 
-<h1>Bem vindo ao curso JSP</h1>
+	<h1>Bem vindo ao curso JSP</h1>
 
 
-<form action="ServletLogin" method="post">
-	<input name="nome">
-	<input name="idade">
-	
-	<input type="submit" value="Enviar">
-</form>
+	<form action="ServletLogin" method="post">
+
+		<table>
+			<tr>
+				<td><label>Login: </label></td>
+				<td><input name="login" type="text"></td>
+			</tr>
+			<tr>
+				<td><label>Senha: </label></td>
+				<td><input name="senha" type="password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Enviar"></td>
+			</tr>
+		</table>
+		
+	</form>
+	<h4>${msg}</h4>
 
 </body>
 </html>
