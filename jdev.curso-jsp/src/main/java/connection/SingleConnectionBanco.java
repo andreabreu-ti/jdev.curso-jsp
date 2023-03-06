5eb2a73c94c3680da29c452008b88620ccb63656
aandd <aandd@ABREU_DELL>
2023-03-06 19:53:50 -0300

    Módulo 22: 22.16-Conexão SqlServer

--- jdev.curso-jsp/src/main/java/connection/SingleConnectionBanco.java
+++ jdev.curso-jsp/src/main/java/connection/SingleConnectionBanco.java
@@ -5,12 +5,38 @@ import java.sql.DriverManager;
 
 public class SingleConnectionBanco {
 
-	private static String banco = "jdbc:postgresql://localhost:5432/jdev.cursojsp?autoReconnect=true";
-	private static String user = "postgres";
-	private static String senha = "alfa.123";
+//	private static String banco = "jdbc:postgresql://localhost:5432/jdev.cursojsp?autoReconnect=true";
+	private static String banco = "jdbc:sqlserver://localhost:1433;databaseName=jdev-curso-jsp;integratedSecurity=false;encrypt=true;trustServerCertificate=true;";
+	private static String user = "jdev";
+	private static String senha = "123456";
 	private static Connection connection = null;
 	
+	public static Connection getConnection() {
+		return connection;
+	}
+	
 	static {
+		conectar();
+	}
+	
+	public SingleConnectionBanco() { //Quando tiver uma instância vai conectar
+		conectar();
+	}
+	
+	
+	private static void conectar() {
+		try {
+			if(connection == null) {
+				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); //Carrega o drive de conexão do banco
+				connection = DriverManager.getConnection(banco, user, senha);
+				connection.setAutoCommit(false);//Para não efetuar alterações no banco sem nosso comando.
+			}
+		} catch (Exception e) {
+			e.printStackTrace(); //Mostrar qualquer erro no momento de conectar
+		}
+	}
+	
+	/*static {
 		conectar();
 	}
 	
@@ -34,6 +60,6 @@ public class SingleConnectionBanco {
 
 	public static Connection getConnection() {
 		return connection;
-	}
+	}*/
 	
 }
